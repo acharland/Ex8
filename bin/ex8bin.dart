db49d2048230348092ecfe796a5fcbdd47859202
Alexandre Charland <tilex1992@hotmail.com>
2014-11-11 16:23:57 -0500

    Update ex8bin.dart

--- bin/ex8bin.dart
+++ bin/ex8bin.dart
@@ -19,27 +19,37 @@ print(' The association that starts by $letter is: $a2');
 }
 void ex08_2(){
   print('');
-print('Q2');
-print('');
-add(var association_name, var first_name, var last_name, var email, var member){
-member.add({
-'associationn_name': association_name,
-'first_name': first_name,
-'last_name': last_name,
-'email': email,
-});
-Sorting1(var member){
-member.sort((x, y) => x['last_name'].compareTo(y['first_name']));
-print('The list with the addition of Mathieu Martin:');
-print('');
-print_row(member);
-}
-Sorting1(member);
-}
-add('CS', 'Mathieu', 'Martin', 'md@gmail.com', member);
-}
-// Unable to do the removal and the update
-void main() { // Inspired by a colleague, Mr Olivier Tremblay
+  print('Q2');
+  print('');
+  List canadiens = new List();
+  var team1 = new Team("MTL", "Montreal Canadiens", "Players");
+  var player1 = new Player("MTL", "Carey", "Price", "cprice@canadiens.com");
+  canadiens.add(player1);
+  var player2 = new Member("MTL", "David", "Desharnais", "ddesharnais@canadiens.com");
+   canadiens.add(player2);
+  var player3 = new Member("MTL", "Tomas", "Plekanec", "tplekanec@canadiens.com");
+    canadiens.add(player3);
+  List bruins = new List();
+  var team2 = new Team("BOS", "Boston Bruins", "Players");
+   var player4 = new Member("BOS", "Zdeno", "Chara", "zchara@bruins.com");
+   bruins.add(player4);
+   var player5 = new Member("BOS", "Milan", "Lucic", "mlucic@bruins.com");
+    bruins.add(player5);
+   var player6 = new Member("BOS", "Patrice", "Bergeron", "pbergeron@nhl.com");
+     bruins.add(player6);
+// 2.2 Provide the maintenance of the model by providing additions, removals and updates of entities.
+  var player7 = new Member("BOS", "Brad", "Marchand", "bmarchand@bruins.com");
+  bruins.add(player7);
+  team2.addMember(player7);
+  team1.deleteMember(Marchand); //ne semble pas fonctionner??
+  team2.editMember("BOS", "Zdeno", "Chara", "zchara@captain-bruins.com"); 
+  
+  print('$team1 $canadiens');
+ print('');
+ print('$team2 $bruins');
+ } 
+
+void main() { // Inspiré d'un collègue, Monsieur Olivier Tremblay
 ex08_1(); 
 ex08_2(); 
 }
